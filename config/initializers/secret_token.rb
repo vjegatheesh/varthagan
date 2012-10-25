bd7ba509dc347424aab9ac252c69b997434dac23
vjegatheesh <vjegatheesh@gmail.com>
2012-10-25 11:53:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Varthagan::Application.config.secret_token = '57dbb4e9cf1ff5294b8c466ab405d41561a39d93e0019aecbebe368f86e79fa0ebff0b722a44e6a1231977509c31b0fd9f0c6412d868f58db202c25e6fb72e74'
